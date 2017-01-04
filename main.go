0bc9dc318ea1f586845f5973d0e31b917785f17d
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-04 18:38:40 +0900

    Receive SendGrid event webhook and parse

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -9,12 +11,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// SendGridEvents represents the scheme of Event Webhook body
+// https://sendgrid.com/docs/API_Reference/Webhooks/event.html#-Event-POST-Example
+type SendGridEvents []struct {
+	SGMessageID string `json:"sg_message_id"`
+	Email       string `json:"email"`
+	Timestamp   int    `json:"timestamp"`
+	SMTPID      string `json:"smtp-id,omitempty"`
+	Event       string `json:"event"`
+	Category    string `json:"category,omitempty"`
+	URL         string `json:"url,omitempty"`
+	AsmGroupID  int    `json:"asm_group_id,omitempty"`
+}
+
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", r.UserAgent())
+	fmt.Fprintf(w, "https://github.com/dtan4/sendgrid2datadog")
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	var events SendGridEvents
+
+	if err := json.Unmarshal(body, &events); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	for _, event := range events {
+		fmt.Printf("%q\n", event.Event)
+	}
 }
 
 func main() {
@@ -22,6 +58,7 @@ func main() {
 
 	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(rootHandler))).Methods("GET")
 	r.Handle("/ping", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(pingHandler))).Methods("GET")
+	r.Handle("/webhook", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(webhookHandler))).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
