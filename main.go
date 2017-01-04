754bf97906506e392d6dcc88ab8e9d29839c8cef
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-04 17:55:38 +0900

    Implement very simple HTTP server

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s", r.UserAgent())
+}
+
 func main() {
-	fmt.Println("sendgrid2datadog")
+	r := http.NewServeMux()
+
+	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(rootHandler)))
+
+	http.ListenAndServe(":8080", r)
 }
