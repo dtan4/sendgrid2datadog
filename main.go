44215e56fe614bf59ef089926154b7ffdef3525c
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-04 18:08:22 +0900

    Use gorilla/mux

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,10 +18,10 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := http.NewServeMux()
+	r := mux.NewRouter()
 
-	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(rootHandler)))
-	r.Handle("/ping", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(pingHandler)))
+	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(rootHandler))).Methods("GET")
+	r.Handle("/ping", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(pingHandler))).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
 }
